37e95f01c98cbc1c02e069da19340ff7085b6ea8
Maxim Volkov <vlkvmaksim69@yandex>
2023-03-01 00:52:59 +0300

    Решение задачи № 2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,21 @@
+﻿////Решение задачи №2////
+Console.Write("Введите первое число :");
+string A = Console.ReadLine()??"";
+int NumA = 0;
+if (A !=null)
+    NumA = int.Parse(A);
+Console.Write("Введите второе число :");
+   string B = Console.ReadLine()??"";
+int NumB = 0;
+if (B !=null)
+    NumB = int.Parse(B);
+
+if (NumA > NumB)
+{
+    Console.WriteLine($"a={NumA};b={NumB} -> max={NumA}");
+}
+
+else
+{
+    Console.WriteLine($"a={NumA};b={NumB} -> max={NumB}");
+}
