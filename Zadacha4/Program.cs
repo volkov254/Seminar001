180f2b87fc63e266d6f25108358a765dea0d71e7
Maxim Volkov <vlkvmaksim69@yandex>
2023-03-01 01:02:59 +0300

    Решение задачи № 4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите 3 числа:");
+int NumA = Convert.ToInt32(Console.ReadLine());
+int NumB = Convert.ToInt32(Console.ReadLine());
+int NumC = Convert.ToInt32(Console.ReadLine());
+
+int max = NumA;
+Console.WriteLine($"{NumA};{NumB};{NumC} -> max={NumA}");
+if (NumB > max)
+{
+    max = NumB;
+}
+else
+{
+    Console.WriteLine($"{NumA};{NumB};{NumC} -> max={NumB}");
+}
+if (NumC > max)
+{
+    max = NumC;
+}
+else
+{
+    Console.WriteLine($"{NumA};{NumB};{NumC} -> max={NumC}");
+}
