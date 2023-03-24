d5aafcf429a9b5be27d2fb71d3e6f0a76e8a54c2
Maxim Volkov <vlkvmaksim69@yandex>
2023-03-25 00:41:06 +0300

    Добавили задачу №64

---
+++ Zadacha064/Program.cs
@@ -0,0 +1,23 @@
+﻿// // Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+//натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int N = InputNumbers("Введите N: ");
+int count = 2;
+PrintNumber(N, count);
+Console.WriteLine();
+
+void PrintNumber(int N, int count)
+{
+  if (count > N) return;
+  PrintNumber(N, count + 1);
+  Console.Write(count + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
